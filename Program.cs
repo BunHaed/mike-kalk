398ebbaceec13ffa3494b9e4ddffe64ee1426cb4
BH <marius234582@gmail.com>
2023-05-02 21:36:41 +0200

    Final final final

--- Program.cs
+++ Program.cs
@@ -141,7 +141,6 @@ internal class Program
                     }
 
                     else
-                        Console.ReadLine();
                         break;    
                     }
                 break;
